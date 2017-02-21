407979aea622b44e8f536e9b0aea723a2f8ae53f
PaulWAllan <allanpaulw@gmail.com>
2017-02-21 12:54:17 -0800

    Pushing most of the screens we'll need. Still working on the layout. SplashScreen is currently launching the Menu.

--- BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SplashScreen.java
+++ BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SplashScreen.java
@@ -1,13 +1,34 @@
 package ca.sfu.epsilon.bomblocator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static ca.sfu.epsilon.bomblocator.R.id.btnLaunchGame;
 
 public class SplashScreen extends AppCompatActivity {
+
+    final int REQUEST_CODE_NEWGAME = 666;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        setupStartButton();
     }
+
+    private void setupStartButton() {
+        Button startAddPot = (Button) findViewById(btnLaunchGame);
+            startAddPot.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent mainMenuIntent = MainMenu.makeIntent(SplashScreen.this);
+                    startActivityForResult(mainMenuIntent, REQUEST_CODE_NEWGAME);
+                }
+            });
+    }
+
 }
