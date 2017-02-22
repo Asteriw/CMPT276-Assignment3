c3e129d3087d1b5b8623bdafabd9313145673245
kenneyw <kenneyw@sfu.ca>
2017-02-22 14:12:19 -0800

    Did more work on the Settings page. You are now able to know which settings have been selected. The save button is now a back button.

--- BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SettingsScreen.java
+++ BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SettingsScreen.java
@@ -4,22 +4,22 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-
-import java.lang.reflect.Array;
 
 public class SettingsScreen extends AppCompatActivity {
 
     private static final String SHAREDPREF_SET = "BombLocator";
     private static final int SHAREDPREF_ITEM_HIGHSCORE = 100;
     private static final int SHAREDPREF_ITEM_GRIDSIZE = 24;
+    int size = 0;
+    int mines = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +35,13 @@ public class SettingsScreen extends AppCompatActivity {
         Spinner BSizeSpinner = (Spinner) findViewById(R.id.BSizeSpinner);
         ArrayAdapter<CharSequence> BSizeAdapter = ArrayAdapter.createFromResource(this, R.array.board_sizes, android.R.layout.simple_spinner_item);
         BSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        BSizeSpinner.setOnItemSelectedListener(new SpinnerListener());
         BSizeSpinner.setAdapter(BSizeAdapter);
 
         Spinner MineNumSpinner = (Spinner) findViewById(R.id.MineNumSpinner);
         ArrayAdapter<CharSequence> MineNumAdapter = ArrayAdapter.createFromResource(this, R.array.mine_amounts, android.R.layout.simple_spinner_item);
         MineNumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        MineNumSpinner.setOnItemSelectedListener(new SpinnerListener());
         MineNumSpinner.setAdapter(MineNumAdapter);
     }
 
@@ -63,13 +65,53 @@ public class SettingsScreen extends AppCompatActivity {
     }
 
     private void setupSaveButton() {
-        Button saveButton = (Button) findViewById(R.id.savebutton);
-        saveButton.setOnClickListener(new View.OnClickListener() {
+        Button backButton = (Button) findViewById(R.id.backbutton);
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.i("Settings", "It should have saved the options.");
             }
         });
+    }
+
+    private class SpinnerListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
+            int selectedSettingNum = parent.getSelectedItemPosition();
+            String selectedSettingName = parent.getItemAtPosition(position).toString();
+            String adapterName = parent.getAdapter().toString();
+            Log.i("Settings", "Setting number: " + selectedSettingNum + " is: " + selectedSettingName + ".");
+            Log.i("Settings", "Adapter name is: " + adapterName);
+            if (adapterName.equals("android.widget.ArrayAdapter@4ca88ce")) {
+                if (selectedSettingNum == 0) {
+                    size = 0;
+                }
+                if (selectedSettingNum == 1) {
+                    size = 1;
+                }
+                if (selectedSettingNum == 2) {
+                    size = 2;
+                }
+            }
+            else if (adapterName.equals("android.widget.ArrayAdapter@d3f33ef")) {
+                if (selectedSettingNum == 0) {
+                    mines = 0;
+                }
+                if (selectedSettingNum == 1) {
+                    mines = 1;
+                }
+                if (selectedSettingNum == 2) {
+                    mines = 2;
+                }
+                if (selectedSettingNum == 3) {
+                    mines = 3;
+                }
+            }
+            Log.i("Settings", "Size and Mines are: " + size + " and " + mines);
+        }
+
+        public void onNothingSelected (AdapterView<?> parent) {
+        }
     }
 
     public static Intent makeIntent(Context context) {
