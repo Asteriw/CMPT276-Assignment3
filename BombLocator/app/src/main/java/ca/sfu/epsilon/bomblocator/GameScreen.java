0f46bdf90a6c42b5336566dc30cb5e0e5a2fa15f
Paul <allanpaulw@gmail.com>
2017-02-22 22:33:01 -0800

    The photos now show up when you click on the bombs. For whatever reason, the app crashes when you update textviews...

--- BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/GameScreen.java
+++ BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/GameScreen.java
@@ -2,12 +2,21 @@ package ca.sfu.epsilon.bomblocator;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static ca.sfu.epsilon.bomblocator.R.mipmap.bomb;
 
 public class GameScreen extends AppCompatActivity {
 
@@ -19,9 +28,13 @@ public class GameScreen extends AppCompatActivity {
     private final int COLS = 6;
     private final int MINE_COUNT = 12;
     MineArray mineArray = new MineArray(ROWS, COLS);
-
     TotalArray totalArray = new TotalArray(ROWS, COLS);
+    Button[][] buttonArray = new Button[ROWS][COLS];
+    //TextView defuserAmount = (TextView) findViewById(R.id.defuserField);
+    //TextView highScoreTV = (TextView) findViewById(R.id.scoreField);;
 
+    int scans = 0;
+    final int highScore = SHAREDPREF_ITEM_HIGHSCORE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +42,13 @@ public class GameScreen extends AppCompatActivity {
         setContentView(R.layout.activity_game_screen);
         setupArrays();
         setupButtons();
+       // setupTextViews();
     }
+
+   // private void setupTextViews() {
+   //     defuserAmount.setText(scans);
+   //     highScoreTV.setText(highScore);
+   // }
 
     private void setupButtons() {
         TableLayout table = (TableLayout) findViewById(R.id.gameTable);
@@ -38,17 +57,60 @@ public class GameScreen extends AppCompatActivity {
             tableRow.setLayoutParams(new TableLayout.LayoutParams(
                     TableLayout.LayoutParams.MATCH_PARENT,
                     TableLayout.LayoutParams.MATCH_PARENT,
-                    1.0f
+                    2.0f
                 ));
             table.addView(tableRow);
             for (int j = 0; j< COLS; j++){
+                final int FINAL_ROW = i;
+                final int FINAL_COL = j;
                 Button button = new Button(this);
                 button.setLayoutParams(new TableRow.LayoutParams(
                         TableRow.LayoutParams.MATCH_PARENT,
                         TableRow.LayoutParams.MATCH_PARENT,
                         1.0f
                 ));
+                button.setPadding(0,0,0,0);
+                button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        buttonClicked(FINAL_ROW, FINAL_COL);
+                    }
+                });
                 tableRow.addView(button);
+                buttonArray[i][j] = button;
+            }
+        }
+    }
+
+    private void buttonClicked(int row, int col) {
+        //lock button sizes
+        lockButtonSizes();
+        if(mineArray.getValue(row,col) == 1){
+            Button button = buttonArray[row][col];
+            int tempWidth = button.getWidth();
+            int tempHeight = button.getHeight();
+            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), ca.sfu.epsilon.bomblocator.R.mipmap.bomb);
+            Bitmap scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, tempWidth, tempHeight, true);
+            Resources resource = getResources();
+            button.setBackground(new BitmapDrawable(resource, scaledBitmap));
+        } else {
+            scans++;
+            //defuserAmount.setText(scans);
+        }
+    }
+
+    private void lockButtonSizes() {
+        for (int i = 0; i < ROWS; i++){
+            for (int j = 0; j< COLS; j++){
+                Button button = buttonArray[i][j];
+
+                int tempWidth = button.getWidth();
+                button.setMaxWidth(tempWidth);
+                button.setMinWidth(tempWidth);
+
+                int tempHeight = button.getHeight();
+                button.setMaxHeight(tempHeight);
+                button.setMinHeight(tempHeight);
             }
         }
     }
