e69542253fbf3e7d1e609df8570bcea91ef334af
kenneyw <kenneyw@sfu.ca>
2017-02-22 18:53:02 -0800

    The settings page is now able to change the default setting of the spinner values upon starting the activity. All that's left is to save the desired default values of the spinners and wire them up to this function.

--- BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SettingsScreen.java
+++ BombLocator/app/src/main/java/ca/sfu/epsilon/bomblocator/SettingsScreen.java
@@ -8,18 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 public class SettingsScreen extends AppCompatActivity {
 
     private static final String SHAREDPREF_SET = "BombLocator";
     private static final int SHAREDPREF_ITEM_HIGHSCORE = 100;
     private static final int SHAREDPREF_ITEM_GRIDSIZE = 24;
-    int size = 0;
-    int mines = 0;
+    int size = 2;
+    int mines = 3;
+    int initialsize = 0;
+    int initialmines = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +39,16 @@ public class SettingsScreen extends AppCompatActivity {
         Spinner BSizeSpinner = (Spinner) findViewById(R.id.BSizeSpinner);
         ArrayAdapter<CharSequence> BSizeAdapter = ArrayAdapter.createFromResource(this, R.array.board_sizes, android.R.layout.simple_spinner_item);
         BSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        BSizeSpinner.setOnItemSelectedListener(new SpinnerListener());
+        BSizeSpinner.setOnItemSelectedListener(new SpinnerListener1());
         BSizeSpinner.setAdapter(BSizeAdapter);
+        BSizeSpinner.setSelection(size);
 
         Spinner MineNumSpinner = (Spinner) findViewById(R.id.MineNumSpinner);
         ArrayAdapter<CharSequence> MineNumAdapter = ArrayAdapter.createFromResource(this, R.array.mine_amounts, android.R.layout.simple_spinner_item);
         MineNumAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        MineNumSpinner.setOnItemSelectedListener(new SpinnerListener());
+        MineNumSpinner.setOnItemSelectedListener(new SpinnerListener2());
         MineNumSpinner.setAdapter(MineNumAdapter);
+        MineNumSpinner.setSelection(mines);
     }
 
     public void setupReset() {
@@ -74,38 +80,53 @@ public class SettingsScreen extends AppCompatActivity {
         });
     }
 
-    private class SpinnerListener implements AdapterView.OnItemSelectedListener {
+    private class SpinnerListener1 implements AdapterView.OnItemSelectedListener {
 
         public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
+            if (initialsize == 0) {
+                initialsize++;
+                return;
+            }
             int selectedSettingNum = parent.getSelectedItemPosition();
             String selectedSettingName = parent.getItemAtPosition(position).toString();
-            String adapterName = parent.getAdapter().toString();
-            Log.i("Settings", "Setting number: " + selectedSettingNum + " is: " + selectedSettingName + ".");
-            Log.i("Settings", "Adapter name is: " + adapterName);
-            if (adapterName.equals("android.widget.ArrayAdapter@4ca88ce")) {
-                if (selectedSettingNum == 0) {
-                    size = 0;
-                }
-                if (selectedSettingNum == 1) {
-                    size = 1;
-                }
-                if (selectedSettingNum == 2) {
-                    size = 2;
-                }
+            Log.i("Settings", "Size number: " + selectedSettingNum + " is: " + selectedSettingName + ".");
+            if (selectedSettingNum == 0) {
+                size = 0;
             }
-            else if (adapterName.equals("android.widget.ArrayAdapter@d3f33ef")) {
-                if (selectedSettingNum == 0) {
-                    mines = 0;
-                }
-                if (selectedSettingNum == 1) {
-                    mines = 1;
-                }
-                if (selectedSettingNum == 2) {
-                    mines = 2;
-                }
-                if (selectedSettingNum == 3) {
-                    mines = 3;
-                }
+            if (selectedSettingNum == 1) {
+                size = 1;
+            }
+            if (selectedSettingNum == 2) {
+                size = 2;
+            }
+            Log.i("Settings", "Size and Mines are: " + size + " and " + mines);
+        }
+
+        public void onNothingSelected (AdapterView<?> parent) {
+        }
+    }
+
+    private class SpinnerListener2 implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected (AdapterView<?> parent, View view, int position, long id) {
+            if (initialmines == 0) {
+                initialmines++;
+                return;
+            }
+            int selectedSettingNum = parent.getSelectedItemPosition();
+            String selectedSettingName = parent.getItemAtPosition(position).toString();
+            Log.i("Settings", "Mines number: " + selectedSettingNum + " is: " + selectedSettingName + ".");
+            if (selectedSettingNum == 0) {
+                mines = 0;
+            }
+            if (selectedSettingNum == 1) {
+                mines = 1;
+            }
+            if (selectedSettingNum == 2) {
+                mines = 2;
+            }
+            if (selectedSettingNum == 3) {
+                mines = 3;
             }
             Log.i("Settings", "Size and Mines are: " + size + " and " + mines);
         }
